95671a6716045d20ff4b2a2e6ade8ff31823fdbe
Abramov Viacheslav <aslava@mail.ru>
2023-07-11 16:34:48 +0300

    fixture added

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -1,9 +1,13 @@
 from utils.dicts import get_val
+import pytest
 
-def test_get_val():
-    data = {"vcs": "mercurial"}
-    assert get_val(data, "vcs") == "mercurial"
-    assert get_val(data, "vcs", "git") == "mercurial"
-    data = {}
-    assert get_val(data, "vcs", "git") == "git"
+@pytest.fixture
+def get_dict():
+    return {"vcs": "mercurial"}
+
+
+def test_get_val(get_dict):
+    assert get_val(get_dict, "vcs") == "mercurial"
+    assert get_val(get_dict, "vcs", "git") == "mercurial"
+    assert get_val({}, "vcs", "git") == "git"
     assert get_val({}, "vcs", "bazaar") == "bazaar"
